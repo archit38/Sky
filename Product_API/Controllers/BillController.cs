1d4ff0e22c8687381dc8dffa9a62a7b128f542bd
archit38 <architbhardwaj88@gmail.com>
2015-11-12 14:35:11 +0000

    first
    commit

---
+++ Product_API/Controllers/BillController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Bill.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Bill.Controllers
+{
+    public class BillController : ApiController
+    {
+        public readonly string URL = "http://safe-plains-5453.herokuapp.com/bill.json";
+
+        [HttpGet]
+        public HttpResponseMessage GetBill()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string billJson = client.DownloadString(URL);
+                var bill = JsonConvert.DeserializeObject<Bill.Models.Bill>(billJson);
+                if (bill == null)
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                return Request.CreateResponse(HttpStatusCode.OK, bill);     
+            }
+        }
+    }
+}
---
+++ Product_API/Models/Bill.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bill.Models
+{
+    public enum SubscriptionType
+    {
+        TV = 1,
+        Talk = 2,
+        Broadband = 3
+    }
+
+    public enum StoreType
+    {
+        Rental = 1,
+        BuyAndKeep = 2
+    }
+
+    public class Bill
+    {
+        public Statement Statement { get; set; }
+        public CallCharges CallCharges { get; set; }
+        public Package Package { get; set; }
+        public SkyStore SkyStore { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class Statement
+    {
+        public DateTime Generated { get; set; }
+        public DateTime Due { get; set; }
+        public Period Period { get; set; }
+    }
+
+    public class Period
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+
+    public class CallCharges
+    {
+        public List<Call> Calls {get;set;}
+        public decimal Total { get; set; }
+    }
+
+    public class Call
+    {
+        public string Called { get; set; }
+        public string duration { get; set; }
+        public decimal Cost { get; set; }
+    }
+
+    public class Package
+    {
+        public List<Subscription> Subscriptions { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class Subscription
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+    }
+
+    public class SkyStore
+    {
+        public List<Rental> Rentals { get; set; }
+        public List<BuyAndKeep> BuyAndKeep { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public abstract class Store
+    {
+        public abstract string Title { get; set; }
+        public abstract decimal Cost { get; set; }
+    }
+
+    public class Rental : Store
+    {
+        public override string Title { get; set; }
+        public override decimal Cost { get; set; }
+    }
+
+    public class BuyAndKeep : Store
+    {
+        public override string Title { get; set; }
+        public override decimal Cost { get; set; }
+    }
+}
